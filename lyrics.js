544be57c356b02c29cca768457391f2e7ef7e19c
Rich <111841072+whozrich@users.noreply.github.com>
2026-02-08 12:48:04 -0500

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,11 @@
+window.TRM_LYRICS = {
+    "decision-made": `
+        First line of the song
+        Second line of the song
+        Look, no extra quotes or commas!
+        It just works.
+    `,
+    "another-track": `
+        Lyrics go here...
+    `
+};
